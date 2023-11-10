5aaf2b41eec0699f60dfe821424d78b6cce6824c
louien-hub <louienfrancoaxalan@gmail.com>
2023-11-09 23:18:08 -0800

    all update

--- exercises/main.js
+++ exercises/main.js
@@ -8,8 +8,22 @@ textArea.addEventListener('input', function() {
 function result_act1() {
     var textArea = document.getElementById("act1").value;
 
-    var data = "<main></main>";
+    var data = 
+    `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+</head>
+<body>
+</body>
+</html>
+
+    `;
     textArea = textArea.replace(/\s/g, '');
+    data = data.replace(/\s/g, '');
 
     if (textArea === data) {
         document.getElementById('show-result').innerHTML = "Correct";
